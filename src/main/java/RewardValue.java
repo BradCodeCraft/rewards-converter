66c2c9ad48176e50b243d8db2cfa45da4ea4df9f
BradCodeCraft <bradcodecraft@gmail.com>
2023-07-28 11:21:46 -0400

    Created RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,14 +1,24 @@
 public class RewardValue {
-    public Main(int cashValue, int milesValue) {
-        cash = cashValue;
-        miles = milesValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+    
+    public RewardValue(double cashValue, int milesValue) {
+        this.cash = cashValue;
+        this.miles = milesValue;
     }
 
-    static void getCashValue(RewardValue input) {
-        return input.miles / 0.0035;
+    private static double convertToCash(RewardValue input) {
+        return this.miles * MILES_TO_CASH_CONVERSION_RATE;
     }
 
-    static void getMilesValue(RewardValue input) {
-        return input.cash / 0.0035;
+    private static int convertToMiles(RewardValue input) {
+        return (int) this.cash / MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return convertToMiles(this.cash)
     }
 }
